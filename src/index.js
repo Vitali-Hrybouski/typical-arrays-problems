aa864917865bcb79d31a714ef5dfd8d169325a42
Vitali-Hrybouski <60929162+Vitali-Hrybouski@users.noreply.github.com>
2020-03-03 12:27:22 +0300

    feat: add solution task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (Array.isArray(array) == true) {
+    if (array.length == 0) return 0;
+    else return array.sort((a, b) => (a - b)).shift();
+  }
+  else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (Array.isArray(array) == true) {
+    if (array.length == 0) return 0;
+    else return array.sort((a, b) => (a - b)).pop();
+  }
+  else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (Array.isArray(array) == true) {
+    if (array.length == 0) {
+      console.log(array);
+      return 0;
+    }
+    else {
+      let sum = 0;
+      array.forEach(element => {
+        sum += element;
+      });
+      let result = sum / array.length;
+      return result;
+    }
+  }
+  else return 0;
 }
+
